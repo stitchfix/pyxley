565cd17bdfd21f31f8c0047748204e6c5bc62491
Nicholas Kridler <nkridler@nmkridler.local>
2015-09-02 09:33:53 -0700

    added labelType option to pie_chart; this allows key, value, percent displays

--- pyxley/charts/nvd3/pie_chart.py
+++ pyxley/charts/nvd3/pie_chart.py
@@ -3,15 +3,21 @@ from flask import jsonify, request
 import numpy as np
 
 class PieChart(NVD3):
+    _allowed = ["key", "value", "percent"]
     def __init__(self, values, df, init_params={},
-        chart_id="piechart", url="/piechart/", colors=[]):
+        chart_id="piechart", url="/piechart/", colors=[],
+        label_type="percent"):
+
+        if label_type not in self._allowed:
+            label_type = "percent"
 
         self.options = {
             "type": "PieChart",
             "chartid": chart_id,
             "url": url,
             "colors": colors,
-            "init_params": init_params
+            "init_params": init_params,
+            "labelType": label_type
         }
 
         self.values = values
