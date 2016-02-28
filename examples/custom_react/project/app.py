950f8b808f1951d958c0971e67d67d443bc0341f
Nicholas Kridler <nkridler@nmkridler.attlocal.net>
2016-02-28 15:50:32 -0800

    updated app to use webpack instead of pyreact

--- examples/custom_react/project/app.py
+++ examples/custom_react/project/app.py
@@ -7,8 +7,6 @@ import glob
 import numpy as np
 import argparse
 
-from react import jsx
-
 from pyxley import UILayout
 from pyxley.filters import SelectButton
 from pyxley.charts import Chart
@@ -23,37 +21,21 @@ args = parser.parse_args()
 TITLE = "Pyxley"
 
 scripts = [
-    "./bower_components/jquery/dist/jquery.min.js",
-    "./bower_components/d3/d3.min.js",
-    "./bower_components/nvd3/build/nv.d3.js",
-    "./chartfunc.js",
-    "./require.min.js",
-    "./bower_components/react/react.js",
-    "./bower_components/react-bootstrap/react-bootstrap.min.js",
-    "./bower_components/pyxley/build/pyxley.js",
+    "./chartfunc.js"
 ]
 
 css = [
-    "./bower_components/bootstrap/dist/css/bootstrap.min.css",
-    "./bower_components/nvd3/build/nv.d3.min.css",
     "./css/main.css"
 ]
-
-transformer = jsx.JSXTransformer()
-jsx_input = "static/jsx/"
-jsx_output = "static/js/"
-
-for f in glob.glob(jsx_input+"*.js"):
-    transformer.transform(f,js_path=jsx_output+f.split('/')[-1])
 
 # Make a UI
 ui = UILayout(
     "RunLayout",
-    "./static/js/custom.js",
+    "./jsx/custom",
     "component_id")
 
 # Read in the data and stack it, so that we can filter on columns
-df = pd.read_csv("./static/formatted_run.csv")
+df = pd.read_csv("./project/static/formatted_run.csv")
 
 # Make a Button
 choices = ["Heart Rate", "Pace", "Distance"]
@@ -67,13 +49,17 @@ nc = NewChart("Seconds", "value", "Altitude", df,
 ui.add_chart(nc)
 
 app = Flask(__name__)
-sb = ui.render_layout(app, "./static/layout.js")
+sb = ui.render_layout(app, "./project/static/layout.js")
 
+# Use the webpack file in the dir and bundle our javascript
+from pyxley.utils import Webpack
+wp = Webpack(".")
+wp.run()
 
 @app.route('/', methods=["GET"])
 @app.route('/index', methods=["GET"])
 def index():
-    _scripts = ["./layout.js", "./js/navbar.js"]
+    _scripts = ["./bundle.js"]
     return render_template('index.html',
         title=TITLE,
         base_scripts=scripts,
