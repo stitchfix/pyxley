9bfae0e8b6fe7d92f65d462b2516fec5f22109f7
Nicholas Kridler <nkridler@nmkridler.local>
2016-02-29 11:04:36 -0800

    adding a plotly example

--- examples/plotly/demo/__init__.py
+++ examples/plotly/demo/__init__.py
@@ -12,28 +12,32 @@ here = path.abspath(path.dirname(__file__))
 app = Flask(__name__)
 
 ui = make_ui(here+"/fitbit_data.csv")
-ui.render_layout(app, "./assets/scripts/app.js")
+ui.render_layout(app, "./demo/static/layout.js")
 
-TITLE = "Plotly Example"
+# Create a webpack file and bundle our javascript
+from pyxley.utils import Webpack
+wp = Webpack(".")
+wp.create_webpack_config(
+    "layout.js",
+    "./demo/static/",
+    "bundle",
+    "./demo/static/"
+)
+wp.run()
 
-scripts = [
-    # "./bower_components/jquery/dist/jquery.min.js",
-    "./plotly.js"
-]
 
 css = [
-    "./bower_components/bootstrap/dist/css/bootstrap.min.css",
     "./css/main.css"
 ]
 
 @app.route('/', methods=["GET"])
 def index():
-    _scripts = ["./js/bundle.js"]
+    _scripts = ["./bundle.js"]
     return render_template('index.html',
         page_scripts=_scripts,
-        base_scripts=scripts,
+        base_scripts=[],
         css=css,
-        title=TITLE)
+        title="Plotly Example")
 
 if __name__ == "__main__":
     app.run(debug=True)
--- examples/plotly/webpack.config.js
+++ examples/plotly/webpack.config.js
@@ -1,59 +1,53 @@
-var path = require("path");
-var webpack = require("webpack");
 
-var root = './assets'
+        var path = require("path");
+        var webpack = require("webpack");
 
-module.exports = {
-  entry: [
-      root+"/scripts/app.js"
-  ],
-  output: {
-    path: './demo/static/js',
-    sourceMapFilename: 'js/bundle.map',
-    filename: 'bundle.js'
-  },
-  resolve: {
-      extensions: ['', '.js', '.scss'],
-      alias: {
-          react: path.resolve('./node_modules/react')
-      }
-  },
+        module.exports = {
+          entry: [
+              './demo/static//layout.js'
+          ],
+          output: {
+            path: path.join(__dirname,'./demo/static/'),
+            sourceMapFilename: 'bundle.map',
+            filename: 'bundle.js'
+          },
+          resolve: {
+              extensions: ['', '.js', '.scss'],
+              alias: {
+                  react: path.resolve('./node_modules/react')
+              }
+          },
 
-  module: {
-    loaders: [
-      {
-        test: /\.js?$/,
-        include: path.join(__dirname, './assets/scripts/'),
-        loader: ['babel-loader'],
-        exclude: /node_modules/,
-        query: {
-            presets: ['es2015', 'react']
-        }
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style', 'css', 'sass']
-      },
-      {
-        test: /\.json$/,
-        loader: 'json'
-      }
-    ]
-},
-plugins: [
-
-        // new webpack.optimize.UglifyJsPlugin(),
-        // new webpack.optimize.DedupePlugin(),
-        // new webpack.DefinePlugin({
-        //     "process.env": {
-        //         NODE_ENV: '"production"'
-        //     }
-        // }),
-        new webpack.ProvidePlugin({
-          $: "jquery",
-          jQuery: "jquery"
-        })
-        // new webpack.NoErrorsPlugin()
-
-    ]
-};
+          module: {
+            loaders: [
+              {
+                test: /\.js?$/,
+                include: path.join(__dirname,'./demo/static/'),
+                loader: ['babel-loader'],
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+              },
+              {
+                test: /\.scss$/,
+                loaders: ['style', 'css', 'sass']
+              },
+              {
+                test: /\.json$/,
+                loader: 'json'
+              }
+            ]
+        },
+        plugins: [
+                new webpack.ProvidePlugin({
+                  $: "jquery",
+                  jQuery: "jquery",
+                  MG: "metrics-graphics",
+                  Datamap: "datamaps",
+                  Plotly: "plotly.js",
+                  nv: "nvd3"
+                })
+            ]
+        };
+    
