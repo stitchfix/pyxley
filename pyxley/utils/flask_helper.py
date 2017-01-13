985b7d142c15a58789a485660db31d17382a63c8
Nicholas Kridler <nkridler@ip-10-8-0-98.ec2.internal>
2017-01-13 13:43:18 -0800

    created some default flask app methods so the user doesn't need to create a flask app

---
+++ pyxley/utils/flask_helper.py
@@ -0,0 +1,43 @@
+import os
+from flask import Flask, render_template
+
+DEFAULT_HTML_PARAMS = {
+    "page_scripts": ["bundle.js"],
+    "base_scripts": [],
+    "css": ["./css.main.css"],
+    "title": "Pyxley"
+}
+
+def create_app(instance_path, static_path, template_path,
+    index_params=DEFAULT_HTML_PARAMS, html="index.html"):
+    """
+
+        Args:
+            instance_path (str): top level
+            static_path (str): path to static
+    """
+    app = Flask(__name__, instance_path=instance_path,
+                          instance_relative_config=True,
+                          static_folder=static_path,
+                          template_folder=template_path)
+
+    def index(name="index"):
+        return render_template(html, **index_params)
+    app.add_url_rule("/", view_func=index)
+    app.add_url_rule("/<string:name>", view_func=index)
+
+    return app
+
+def default_static_path():
+    """
+        Return the path to the javascript bundle
+    """
+    fdir = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(fdir, '../../assets/'))
+
+def default_template_path():
+    """
+        Return the path to the index.html
+    """
+    fdir = os.path.dirname(__file__)
+    return os.path.abspath(os.path.join(fdir, '../../assets/templates/'))
